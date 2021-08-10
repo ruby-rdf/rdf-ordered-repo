c73eb936b77905f5e51e9bd6da697cad4500ce29
Gregg Kellogg <gregg@greggkellogg.net>
2021-08-10 11:03:20 -0700

    Improve coverage. Change order of includes in spec_helper.

--- spec/ordered_repo_spec.rb
+++ spec/ordered_repo_spec.rb
@@ -4,7 +4,7 @@ require 'rdf/spec/repository'
 describe RDF::OrderedRepo do
   # @see lib/rdf/spec/repository.rb in rdf-spec
   it_behaves_like 'an RDF::Repository' do
-    let(:repository) { described_class.new }
+    let(:repository) { RDF::OrderedRepo.new }
   end
 
   it { is_expected.not_to be_durable }
@@ -16,5 +16,4 @@ describe RDF::OrderedRepo do
     expect(repository.options).to have_key(:foo)
     expect(repository.options[:foo]).to eq :bar
   end
-
 end
