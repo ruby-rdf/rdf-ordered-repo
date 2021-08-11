e363c2e8475cbb689e13ed5ae560f736a3a0aea8
Gregg Kellogg <gregg@greggkellogg.net>
2021-08-11 12:32:26 -0700

    Remove explicit dependency on coveralls gem.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,6 @@ end
 begin
   require 'simplecov'
   require 'simplecov-lcov'
-  require 'coveralls'
 
   SimpleCov::Formatter::LcovFormatter.config do |config|
     #Coveralls is coverage by default/lcov. Send info results
